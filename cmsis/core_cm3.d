dc0ee5f2137a2160c81ca6002b8dc96e79c4d33b
Vlad Trishch <vlad.trishch@gmail.com>
2019-01-01 00:15:30 +0200

    Set up proper .gitignore

--- cmsis/core_cm3.d
+++
@@ -1,6 +0,0 @@
-cmsis/core_cm3.o: cmsis/core_cm3.c \
- /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
- /usr/include/newlib/stdint.h \
- /usr/include/newlib/machine/_default_types.h \
- /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
- /usr/include/newlib/sys/_stdint.h
--- cmsis/system_stm32f10x.d
+++
@@ -1,8 +0,0 @@
-cmsis/system_stm32f10x.o: cmsis/system_stm32f10x.c cmsis/stm32f10x.h \
- cmsis/core_cm3.h /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
- /usr/include/newlib/stdint.h \
- /usr/include/newlib/machine/_default_types.h \
- /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
- /usr/include/newlib/sys/_stdint.h cmsis/system_stm32f10x.h \
- src/stm32f10x_conf.h stdperiph/inc/stm32f10x_gpio.h cmsis/stm32f10x.h \
- stdperiph/inc/stm32f10x_rcc.h
